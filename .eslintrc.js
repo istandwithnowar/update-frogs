b22b8d0a7991e2fb32df202d018b0ddd79d51ed5
istandwithnowar <137201117+istandwithnowar@users.noreply.github.com>
2023-06-27 20:18:15 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,3 @@
-/*
- * @Author: istandwithnowar 137201117+istandwithnowar@users.noreply.github.com
- * @Date: 2023-06-27 16:25:54
- * @LastEditors: istandwithnowar 137201117+istandwithnowar@users.noreply.github.com
- * @LastEditTime: 2023-06-27 16:26:00
- * @FilePath: /update-frogs/.eslintrc.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 module.exports = {
     root: true,
     env: {
