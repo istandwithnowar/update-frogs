f1c2afc9ad2d46bca606ade7f0ba84d7a41d24d7
istandwithnowar <137201117+istandwithnowar@users.noreply.github.com>
2023-06-27 16:27:39 +0800

    add .file

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: istandwithnowar 137201117+istandwithnowar@users.noreply.github.com
+ * @Date: 2023-06-27 16:25:54
+ * @LastEditors: istandwithnowar 137201117+istandwithnowar@users.noreply.github.com
+ * @LastEditTime: 2023-06-27 16:26:00
+ * @FilePath: /update-frogs/.eslintrc.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: [
+      'plugin:vue/vue3-essential',
+      'eslint:recommended',
+    ],
+  }
